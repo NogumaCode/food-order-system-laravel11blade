520e3abb7a24c7d46358e0f20f5529ebbd3849fb
NogumaCode <111834765+NogumaCode@users.noreply.github.com>
2024-12-02 23:02:17 +0900

    password reset mail send function setting

---
+++ app/Mail/Websitemail.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class Websitemail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $subject, $reset_link;
+
+     /**
+     * Create a new message instance.
+     *
+     * @param string $subject メールの件名
+     * @param string $reset_link リセットリンク
+     */
+    public function __construct(string $subject, string $reset_link)
+    {
+        $this->subject = $subject;
+        $this->reset_link = $reset_link;
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: $this->subject,
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'emails.password_reset',
+            with: [
+                'reset_link' => $this->reset_link,
+            ],
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
---
+++ resources/views/emails/password_reset.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>パスワードリセット</title>
+</head>
+<body>
+    <p>以下のリンクをクリックしてパスワードをリセットしてください。</p>
+    <p><a href="{{ $reset_link }}">{{ $reset_link }}</a></p>
+</body>
+</html>
